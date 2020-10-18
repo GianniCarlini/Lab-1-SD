b60d067839e04ae0cbacdea04b39aa9e45061009
Gianni Carlini <gianni.carlini@sansano.usm.cl>
2020-10-18 11:28:22 -0300

    prueba de rabbit

--- server.go
+++ server.go
@@ -12,6 +12,8 @@ import (
 	"os"
 	"encoding/csv"
 
+	"github.com/streadway/amqp"
+
 	pb "github.com/GianniCarlini/Lab-1-SD/chat"
 	"google.golang.org/grpc"
 )
@@ -319,6 +321,45 @@ func (s *server) Seguimiento(ctx context.Context, in *pb.SeguimientoRequest) (*p
 	}
 
 	return &pb.SeguimientoReply{Estadoseguimiento: esta2,}, nil
+}
+func failOnError(err error, msg string) {
+	if err != nil {
+	  log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func Rabbit(){
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()		
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	body := "Hello World!"
+	err = ch.Publish(
+  	"",     // exchange
+  	q.Name, // routing key	
+  	false,  // mandatory
+  	false,  // immediate
+  	amqp.Publishing {
+    	ContentType: "text/plain",
+	    Body:        []byte(body),
+  	})
+	failOnError(err, "Failed to publish a message")
+
+	fmt.Println("Successfully Published Message yto Queue")
 }
 
 func main() {
